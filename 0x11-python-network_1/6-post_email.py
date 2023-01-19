97b240b49aa66c4a14f0a25d3793855767c8d2b2
Nebyu <nebyouabayneh@gmail.com>
2023-01-19 20:54:49 +0300

    Networking exercise #1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""A  script that:
-- takes in a URL and an email address
-- sends a POST request to the passed URL with the email as a parameter
-- displays the body of the response.
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
 import sys
 import requests
